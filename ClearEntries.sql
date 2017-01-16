ad57c35f30c4c8aa9dc7f7db032fca022cb8f434
Talha Ahmed <talha.ahmed@gmail.com>
2017-01-16 15:53:12 +0500

    problem fixed

--- ClearEntries.sql
+++ ClearEntries.sql
@@ -2,20 +2,20 @@
 
 Declare @code int;
 Declare @fromDate Datetime;
-SET @code=9600503
+SET @code=9600517
 SET @fromDate=CAST('2016-10-18' AS DATETIME)
 
 
 Delete from FPEntries
 --Select * from FPEntries
 	where 
-		--L_UID = @code
-		--and 
+		L_UID = @code
+		and
 		C_Date >= REPLACE(CONVERT(CHAR(20), @fromDate, 102), '.', '') 
 
 Delete from attendancedetails
 --Select * from attendancedetails
 	Where 
-		--EmployeeID = (Select Employeeid from Employee where EmployeeCode=CAST(@code AS VARCHAR))
-		--and
+		EmployeeID = (Select Employeeid from Employee where EmployeeCode=CAST(@code AS VARCHAR))
+		and
 		TrackDate >= @fromDate
